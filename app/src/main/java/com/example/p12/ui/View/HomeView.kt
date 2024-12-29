7bc99998cef7bcfa83586c97a134edb2d48b441b
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-29 21:19:42 +0700

    membuat fungsion onloading dan onrror

--- app/src/main/java/com/example/p12/ui/View/HomeView.kt
+++ app/src/main/java/com/example/p12/ui/View/HomeView.kt
@@ -1,5 +1,7 @@
 package com.example.p12.ui.View
 
+import android.widget.Button
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -22,13 +25,42 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.SemanticsProperties.Text
 import androidx.compose.ui.text.input.KeyboardType.Companion.Text
 import androidx.compose.ui.unit.dp
+import com.example.p12.R
 import com.example.p12.model.Mahasiswa
 import org.w3c.dom.Text
 
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) { Image(
+    modifier = modifier.size(200.dp),
+    painter = painterResource(R.drawable.loading_img), contentDescription = stringResource(R.string.loading)
+)
+}
 
+@Composable
+fun OnError(
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(
+            text = stringResource(R.string.loading_failed),
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
 
 @Composable
 fun MhsLayout(
