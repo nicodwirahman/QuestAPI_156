a88e5ca7d140321a0e6875473695cfb87be6997a
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-29 21:49:40 +0700

    memperbaiki codingan Homeview

--- app/src/main/java/com/example/p12/ui/View/HomeView.kt
+++ app/src/main/java/com/example/p12/ui/View/HomeView.kt
@@ -1,6 +1,6 @@
 package com.example.p12.ui.View
 
-import android.widget.Button
+import androidx.compose.material3.Button
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -16,33 +16,99 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.SemanticsProperties.Text
 import androidx.compose.ui.text.input.KeyboardType.Companion.Text
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.p12.R
 import com.example.p12.model.Mahasiswa
 import com.example.p12.ui.ViewModel.HomeUiState
+import com.example.p12.ui.ViewModel.HomeViewModel
+import com.example.p12.ui.navigasi.CustomeTopAppBar
+import com.example.p12.ui.navigasi.DestinasiNavigasi
 import org.w3c.dom.Text
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (String) -> Unit = {},
+    viewModel: HomeViewModel = viewModel()
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomeTopAppBar(
+                title = DestinasiHome.titleRes,
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior,
+                onRefresh = {
+                    viewModel.getMhs()
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Kontak"
+                )
+            }
+        }
+    ) { innerPadding ->
+        HomeStatus(
+            homeUiState = viewModel.mhsUiState,
+            retryAction = { viewModel.getMhs() },
+            modifier = Modifier.padding(innerPadding),
+            onDetailClick = onDetailClick,
+            onDeleteClick = { mhs ->
+                viewModel.deleteMhs(mhs.nim)
+                viewModel.getMhs()
+            }
+        )
+    }
+}
+
 
 
 @Composable
 fun HomeStatus(
     homeUiState: HomeUiState,
     retryAction: () -> Unit,
-    modifier: Modifier = Modifier, onDeleteClick: (Mahasiswa) -> Unit = {}, onDetailClick: (String) -> Unit
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Mahasiswa) -> Unit = {},
+    onDetailClick: (String) -> Unit
 ) {
 
 
@@ -78,21 +144,25 @@ fun OnLoading(modifier: Modifier = Modifier) { Image(
 )
 }
 
+/**
+ * The home screen displaying error message with re-attempt button.
+ */
+
 @Composable
 fun OnError(
     retryAction: () -> Unit,
     modifier: Modifier = Modifier) {
     Column(
         modifier = modifier,
-        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
         )
         Text(
             text = stringResource(R.string.loading_failed),
-            modifier = Modifier.padding(16.dp)
-        )
+            modifier = Modifier.padding(16.dp))
         Button(onClick = retryAction) {
             Text(stringResource(R.string.retry))
         }
