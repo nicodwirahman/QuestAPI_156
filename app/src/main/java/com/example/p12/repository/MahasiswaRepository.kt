85bd274e8e536707baaaaed0c8b475f07e294a9c
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-29 23:21:14 +0700

    memperbaik code

--- app/src/main/java/com/example/p12/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/p12/repository/MahasiswaRepository.kt
@@ -2,37 +2,48 @@ package com.example.p12.repository
 
 import com.example.p12.model.Mahasiswa
 import com.example.p12.service_api.MahasiswaService
+import okio.IOException
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface MahasiswaRepository {
     suspend fun getMahasiswa(): List<Mahasiswa>
 
-    suspend fun  insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 
     suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
 
-    suspend fun  deleteMahasiswa(nim: String)
+    suspend fun deleteMahasiswa(nim: String)
 
     suspend fun getMahasiswabyNim(nim: String): Mahasiswa
 }
 
 class NetworkMahasiswaRepository(
-    private val MahasiswaApiService: MahasiswaService
-): MahasiswaRepository {
-    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
-        MahasiswaApiService.insertMahasiswa(mahasiswa)
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun getMahasiswa(): List<Mahasiswa> =
+        mahasiswaApiService.getAllMahasiswa()
 
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
     }
 
     override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
-        MahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
     }
 
-    override suspend fun getMahasiswabyNim(): List<Mahasiswa> =
-        kontakApiService.getAllMahasiswa()
+    override suspend fun deleteMahasiswa(nim: String) {
+        mahasiswaApiService.deleteMahasiswa(nim)
+    }
 
-    override suspend fun getMahasiswa(): List<Mahasiswa> {
-        TODO("Not yet implemented")
+    override suspend fun getMahasiswabyNim(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswabyNim(nim)
     }
-    }
-    }
+}
+
 
