fda85b6c71a960b31e515007a903cc6dd7d9067c
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-29 17:30:27 +0700

    Mengambil data mahasiswa dan mengatur status UI.

--- app/src/main/java/com/example/p12/ui/ViewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/p12/ui/ViewModel/HomeViewModel.kt
@@ -1,9 +1,40 @@
 package com.example.p12.ui.ViewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.p12.model.Mahasiswa
+import com.example.p12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomeUiState{
     data class success(val mahasiswa: List<Mahasiswa>): HomeUiState()
     object Error: HomeUiState()
     object  Loading: HomeUiState()
+}
+
+class HomeViewModel(private val mhs: MahasiswaRepository): ViewModel() {
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    private fun getMhs() {
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
 }
