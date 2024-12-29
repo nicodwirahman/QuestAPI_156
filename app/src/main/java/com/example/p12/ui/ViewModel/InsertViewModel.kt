071c5c3ed62dfeaf8b57d73f54e9c9aa66fb4660
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-29 17:16:41 +0700

    InsertViewModel

--- app/src/main/java/com/example/p12/ui/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/p12/ui/ViewModel/InsertViewModel.kt
@@ -4,14 +4,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.p12.model.Mahasiswa
 import com.example.p12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
     var uiState by mutableStateOf(InsertUiState())
         private set
-}
 
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = uiState.copy(insertUiEvent = insertUiEvent)
+    }
+
+   suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 
 data class InsertUiState(
@@ -25,6 +40,15 @@ data class InsertUiEvent(
     val jenisKelamin: String ="",
     val kelas: String ="",
     val angkatan: String =""
+)
+
+fun InsertUiEvent.toMhs():Mahasiswa= Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jeniskelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan,
 )
 
 fun Mahasiswa.toUiStateMhs():InsertUiState = InsertUiState(
