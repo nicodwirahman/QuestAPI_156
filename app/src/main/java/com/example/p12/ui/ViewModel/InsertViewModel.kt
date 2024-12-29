b44ee6b55dd01d6d9b713cfb662c97591e793a74
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-29 17:05:40 +0700

    membuat class insertviewmodel

--- app/src/main/java/com/example/p12/ui/ViewModel/InsertViewModel.kt
+++ app/src/main/java/com/example/p12/ui/ViewModel/InsertViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.p12.ui.ViewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.p12.model.Mahasiswa
+import com.example.p12.repository.MahasiswaRepository
+
+class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+}
+
 
 
 data class InsertUiState(
