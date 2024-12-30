238b78c017377f7944afee4466d4bafc0870e62f
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-30 11:52:02 +0700

    memperbaiki codingan penyediaViewModel

--- app/src/main/java/com/example/p12/ui/ViewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/p12/ui/ViewModel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.p12.ui.ViewModel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -8,11 +9,27 @@ import com.example.p12.MahasiswaApplications
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiKontak().container.mahasiswaRepository) }
-        initializer { InsertViewModel(aplikasiKontak().container.mahasiswaRepository) }
-
+        initializer {
+            HomeViewModel(
+                MahasiswaApplications().container.mahasiswaRepository)
+        }
+        initializer {
+            InsertViewModel(
+                MahasiswaApplications().container.mahasiswaRepository)
+        }
+        initializer {
+            DetailViewModel(
+                createSavedStateHandle(),
+                MahasiswaApplications().container.mahasiswaRepository)
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                MahasiswaApplications().container.mahasiswaRepository)
+        }
     }
-
-    fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
-        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
 }
+
+fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
+
