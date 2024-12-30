afe8673c8f07ab98e2a0e07a19d47a1b84f1eec2
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-30 11:52:23 +0700

    menambah uodta dan detail di pengelola halaman

--- app/src/main/java/com/example/p12/ui/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/p12/ui/navigasi/PengelolaHalaman.kt
@@ -8,8 +8,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.p12.ui.View.DestinasiEntry
 import com.example.p12.ui.View.DestinasiHome
+import com.example.p12.ui.View.DetailScreen
 import com.example.p12.ui.View.EntryMhsScreen
 import com.example.p12.ui.View.HomeScreen
+import com.example.p12.ui.View.UpdateScreen
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
@@ -29,11 +31,29 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                     popUpTo(DestinasiHome.route) {
                         inclusive = true
 
-
                     }
                 }
             })
+
         }
+        composable("detail/{nim}") { backStackEntry ->
+            val nim = backStackEntry.arguments?.getString("nim") ?: ""
+            DetailScreen(
+                nim = nim,
+                navigateBack = { navController.popBackStack() },
+                onUpdateClick = { nim ->
+                    navController.navigate("update/$nim")
+                }
+            )
+        }
+        composable("update/{nim}") { backStackEntry ->
+            val nim = backStackEntry.arguments?.getString("nim") ?: ""
+            UpdateScreen(
+                nim = nim,
+                navigateBack = { navController.popBackStack() }
+            )
     }
 }
+}
+
 
