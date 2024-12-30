52aa5df134c61643500ef7f9b0caec865c1c6983
lenovo <nico.dwi.ft22@mail.umy.c.id>
2024-12-30 11:49:07 +0700

    membuat class updateMhsViewModel

--- app/src/main/java/com/example/p12/ui/ViewModel/updateViewModel.kt
+++ app/src/main/java/com/example/p12/ui/ViewModel/updateViewModel.kt
@@ -9,6 +9,25 @@ import androidx.lifecycle.viewModelScope
 import com.example.p12.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
+class UpdateMhsViewModel(
+    mhsRepository1: SavedStateHandle,
+    private val mhsRepository: MahasiswaRepository
+) : ViewModel() {
+    var uiState by mutableStateOf(UpdateUiState())
+        private set
+
+    // Memuat data Mahasiswa berdasarkan NIM
+
+
+    // Memperbarui UI State berdasarkan event baru
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        uiState = uiState.copy(insertUiEvent = insertUiEvent)
+    }
+}
+
+
+
+
 // State untuk Update Mahasiswa
 data class UpdateUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
